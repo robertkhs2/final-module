530860db0ac493c4b7d8524cc065f87b52f4fe5c
robertkhs2 <dhanushdhanu9809@gmail.com>
2024-05-07 23:27:07 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "robertkhs"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "robert"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "rmt"
 )
 
 var ID ids.ID
